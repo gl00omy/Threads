c7c487afc4c43cf70ad6c0450bb39d34ca3a79e6
Zhibek Tyulemissova <zhibektyulemissova@gmail.com>
2025-01-24 07:40:16 -0800

    multiple threads

--- Threads/Threads.cpp
+++ Threads/Threads.cpp
@@ -5,23 +5,34 @@
 #include <stdio.h>
 
 DWORD WINAPI DoWork(PVOID program) {
+    int data = *(int*)program;
     printf("Worker thread: %u\n", GetCurrentThreadId());
-    Sleep(3000);
+    Sleep(GetCurrentThreadId() / 5);
     printf("Worker done\n");
-    return 42;
+    return data * data;
 }
 
 int main()
 {
-    HANDLE hThread = CreateThread(nullptr, 0, DoWork, nullptr, 0, nullptr);
-
+    int x = 20; 
+    HANDLE hThread = CreateThread(nullptr, 0, DoWork, &x, 0, nullptr);
+    x = 10;
+    HANDLE hThread2 = CreateThread(nullptr, 0, DoWork, &y, 0, nullptr);
+    
     printf("Main thread: %u\n", GetCurrentThreadId());
 
-    WaitForSingleObject(hThread, INFINITE);
-    DWORD code;
-    GetExitCodeThread(hThread, &code);
-    printf("Work done, Result=%u\n", code);
-    CloseHandle(hThread);
+    HANDLE h[] = { hThread, hThread2 };
+    WaitForMultipleObjects(_countof(h), h, TRUE, INFINITE);
+    DWORD sum = 0;
+    for (int i = 0; i < _countof(h); i++) {
+        DWORD code;
+        GetExitCodeThread(h[i], &code);
+        printf("Thread %u is done. Result+%u\n", GetThreadId(h[i]), code);
+        sum += code;
+        CloseHandle(h[i]);
+    }
+
+    printf("Work done, Total=%u\n", sum);
 
     return 0;
 }
